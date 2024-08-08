a023c7d6b0fc6bb629d51704a108f1f69488a6f6
Eduard Muliar <edikmulyar228@gmail.com>
2024-08-08 22:01:50 +0300

    Some fixes to search clean up logic

--- client/src/components/SearchBar.tsx
+++ client/src/components/SearchBar.tsx
@@ -34,6 +34,28 @@ const SearchBar = () => {
     navigate("/search");
   };
 
+  const handleClearSearchBar = (event: FormEvent) => {
+    event.preventDefault();
+
+    const resetDestination = "";
+    const resetCheckIn = new Date();
+    const resetCheckOut = new Date();
+
+    setDestination(resetDestination);
+    setCheckIn(resetCheckIn);
+    setCheckOut(resetCheckOut);
+    setAdultCount(minAdultCount);
+    setChildCount(minChildCount);
+
+    search.saveSearchValues(
+      resetDestination,
+      resetCheckIn,
+      resetCheckOut,
+      minAdultCount,
+      minChildCount
+    );
+  };
+
   const minDate = new Date();
   const maxDate = new Date();
   maxDate.setFullYear(maxDate.getFullYear() + 1);
@@ -111,7 +133,10 @@ const SearchBar = () => {
         <button className="w-2/3 bg-blue-600 text-white h-full p-2 font-bold text-xl hover:bg-blue-500">
           Search
         </button>
-        <button className="w-1/3 bg-red-600 text-white h-full p-2 font-bold text-xl hover:bg-red-500">
+        <button
+          className="w-1/3 bg-red-600 text-white h-full p-2 font-bold text-xl hover:bg-red-500"
+          onClick={handleClearSearchBar}
+        >
           Clear
         </button>
       </div>
